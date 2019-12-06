bda05c9ed1ece6303e1173fac93e932165f242c7
malchul <None>
2019-12-07 00:58:58 +0300

    change dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 sudo docker build -t torch_spark image/
 
-sudo docker run --runtime=nvidia -p 8888:8888 jupyter/pyspark-notebook
+sudo docker run --runtime=nvidia -p 8888:8888 torch_spark
+sudo docker run -p 8888:8888 -v /home/malchul/work/projects/3d_reconstruction/NeighbourNet/data/new_shape_net_only_points/:/home/jovyan/data my_spark_torch
+
